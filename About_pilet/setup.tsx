f03674d77e1a138de962dedd717997869a659d6c
sheema <sheemamehboob91@gmail.com>
2022-07-20 13:41:48 -0400

    Add project files.

---
+++ About_pilet/setup.tsx
@@ -0,0 +1,10 @@
+﻿import { PiletApi } from "../piral~/About_Pilet/node_modules/goodhealth-piral";
+
+export default (app: PiletApi) => {
+    app.registerTile(app.fromBlazor("my-tile"));
+    app.registerMenu(app.fromBlazor("AboutPage-menu"));
+    app.registerMenu(app.fromBlazor("counter"));
+
+
+    /*  app.registerPage("/providers", app.fromBlazor("providers"));   */
+};
